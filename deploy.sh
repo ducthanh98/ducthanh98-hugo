c4ed77ebe441c4f7259e81fe2ba2896066b68fd1
thanhld <thanhld9x.dev@gmail.com>
2022-12-06 18:23:52 +0700

    Updating ...

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,5 @@ set -xe
 echo 'Building ...'
 cd web && hugo -D
 echo 'Committing'
-cd .. && pwd && git add . &&  git commit -m'Updating ...'
-cd ./ducthanh98.github.io && git add . && git commit -m'Updating ...'
-echo 'Pushing ...'
-cd .. && pwd && git push --recurse-submodules=on-demand
+cd .. && pwd && git add . &&  git commit -m'Updating ...' && git push origin main
+cd ./ducthanh98.github.io && git add . && git commit -m'Updating ...' && git push origin main
